07d9e819b3967892915c02f4b1a875ae047dd8e1
Mariam Mena <mariam.ortega@utp.ac.pa>
2022-06-30 15:11:33 -0500

    Se realizaron cambios

--- calcular.php
+++ calcular.php
@@ -3,14 +3,15 @@ $cantidad = $_POST['cantidad'];
 $subtotal = $cantidad*125;
 $numero_cajas = ceil($cantidad/4);
 $flete = $numero_cajas*50;
-if ($cantidad>1000)
+if ($cantidad>1000){
     $descuento=$subtotal*0.15;
+}
 else
 {
-    if($cantidad>100)
-        $descuento=$subtotal*0.05;
-    else
-        $descuento=0;
+    if($cantidad>100){
+        $descuento=$subtotal*0.05;}
+    else{
+        $descuento=0;}
 }
 $total=$subtotal+$flete-$descuento;
 echo "El precio total a pagar es: ".$total."<br>";
